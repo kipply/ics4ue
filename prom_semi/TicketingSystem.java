37c0f61df4296b1cca63e5126925fb713d1a0030
kipply <carolyfisher@gmail.com>
2019-02-12 12:57:13 -0500

    Add id and name generation

--- prom_semi/TicketingSystem.java
+++ prom_semi/TicketingSystem.java
@@ -7,12 +7,20 @@ class TicketingSystem {
   public static void main(String[] args) {
     ArrayList<Student> mockStudents = new ArrayList<Student>(); 
     Random rand = new Random();
-    for (int i = 0; i < 100; i++) { // generate 100 students
+    String[] studentNumbers = new String[100]; 
+
+    for (int i = 0; i < studentNumbers.length; i++) { // generate 100 students
+      studentNumbers[i] = Integer.toString(rand.nextInt(100000000));
+    }
+    for (int i = 0; i < studentNumbers.length; i++) { // generate 100 students
       mockStudents.add(new Student(
-        "My Name", 
-        Integer.toString(rand.nextInt(100000000)), 
+        randName(rand), 
+        studentNumbers[i], 
         new ArrayList<>(Arrays.asList("No Meat", "No dairy")),
-        new ArrayList<>(Arrays.asList(Integer.toString(rand.nextInt(100000000)), Integer.toString(rand.nextInt(100000000))))       
+        new ArrayList<>(Arrays.asList(
+          studentNumbers[rand.nextInt(studentNumbers.length)], 
+          studentNumbers[rand.nextInt(studentNumbers.length)]
+        ))       
       )); 
     }
 
@@ -22,9 +30,21 @@ class TicketingSystem {
     for (Table table : tables) { 
       ArrayList<Student> students = table.getStudents();
       for (Student student : students) { 
-        System.out.println(student.getStudentNumber());
+        System.out.println(student.getName() + " " + student.getStudentNumber());
       }
       System.out.println("----");
     }
   }
+
+  private static String randName(Random random) {
+    String letters = "pyfgcrlaoeuidhtnsqjkxbmwvz"; 
+    int nameLength = random.nextInt(6) + 5;
+    String res = ""; 
+    for (int i = 0; i < nameLength; i++) {
+      res += letters.charAt(random.nextInt(26));
+    }
+    return res;
+  }
+
+
 }
