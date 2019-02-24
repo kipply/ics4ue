5cc049389c7bb2a0c462031c5b905ec1e2ed4a37
kipply <carolyfisher@gmail.com>
2019-02-24 15:48:05 -0500

    mmmm another working version to revert back too if all else goes to shit

--- prom_semi/SeatingAlg.java
+++ prom_semi/SeatingAlg.java
@@ -2,10 +2,19 @@ import java.util.Date;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Random;
 import java.util.Map;
-import java.util.Comparator;
-import java.util.PriorityQueue;
 import java.lang.Math;
+
+import javax.swing.JOptionPane;
+import javax.swing.JFormattedTextField;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.GridBagLayout;
+import java.net.URL;
+import java.net.MalformedURLException;
 
 /**
 * The SeatingAlg class provides methods and tools for generating
@@ -22,64 +31,143 @@ public class SeatingAlg {
   ArrayList<Table> res; 
 
   // Settings
-  private static int maxRuntime = 1; 
-  private static boolean bidirectionalFriendship = true;
+  private static boolean showUISettings = true; 
+  private static int maxRuntime = 5; 
+  private static boolean bidirectionalFriendships = true;
   private static boolean communist = false; 
+  private static boolean special = true; 
 
   /**
   * This method is the driving function for table generation. 
-  * Any settings (ie. setMaxRuntime()) must be set before this method is 
+  * Any settings (ie. maxRuntime()) must be set before this method is 
   * called. 
   * @param students ArrayList of Students to be seated. 
   * @param tableSize  Integer number of students that can bes seating per table. 
   * @return Returns an ArrayList of Table objects. 
   */
   public ArrayList<Table> generateTables(ArrayList<Student> students, int tableSize) {
+    if (showUISettings) { // start getting user input
+
+      String timeInput = null; 
+      while (timeInput == null) {
+        timeInput = JOptionPane.showInputDialog(null, 
+          "Please enter the amount of time you want the algorithm to run(seconds).", 
+          "Seating Algorithm Settings",
+          JOptionPane.INFORMATION_MESSAGE
+        );
+      }
+      maxRuntime = Integer.parseInt(timeInput);
+
+      Object[] friendshipOptions = {"Leave friendships", "Bidirectional"};
+      int n = JOptionPane.showOptionDialog(null,
+                  "Please select whether or not you want to assume friendships are bidirectional. \nAssuming bidirectional is favourable when there's little drama and/or people are lazy to fill in their friends.",
+                  "Seating Algorithm Settings",
+                  JOptionPane.YES_NO_CANCEL_OPTION,
+                  JOptionPane.DEFAULT_OPTION,
+                  null,
+                  friendshipOptions,
+                  friendshipOptions[1]);  
+      bidirectionalFriendships(n == 0);
+
+      Object[] communistOptions = {"Communism", "Capitalism"};
+      n = JOptionPane.showOptionDialog(null,
+                  "Please select which political theory you'd like to enforce to govern happiness over these people. \nSetting to 'communism' will cause the algorithm to optimize for a lower standard deviation (while taking higher scores with very similar standard deviations), \nwhile captalism will optimize for sum of happiness.",
+                  "Seating Algorithm Settings",
+                  JOptionPane.YES_NO_CANCEL_OPTION,
+                  JOptionPane.DEFAULT_OPTION,
+                  null,
+                  communistOptions,
+                  communistOptions[1]);  
+      communist(n == 0);
+
+      Object[] algOptions = {"Simple", "Special"};
+      n = JOptionPane.showOptionDialog(null,
+                  "Please specify which types of calculations you'd like to use. \nSimple simply calculates based on friendsAtTable/totalFriends, however, the complex and recommended\n calculations realizes that person with 20 friends, seated with five friends is likely similarly happy to a \n person with 5 friends seated with 5 friends.",
+                  "Seating Algorithm Settings",
+                  JOptionPane.YES_NO_CANCEL_OPTION,
+                  JOptionPane.DEFAULT_OPTION,
+                  null,
+                  algOptions,
+                  algOptions[1]); 
+      special(n == 1);
+    } // end getting user input
+
+
+    // manage loading screen if applicable 
+    JFrame frame = new JFrame();
+    if (maxRuntime > 5) {
+      JPanel panel = new JPanel();
+      panel.setLayout(new GridBagLayout());
+      
+      URL url = SeatingAlg.class.getResource("/loading.gif");
+      ImageIcon loadingIcon = new ImageIcon(url);
+      JLabel loadGifLabel = new JLabel(loadingIcon);
+      panel.add(loadGifLabel);
+
+      frame.add(panel); 
+      frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+      frame.setUndecorated(true);
+      frame.setVisible(true);
+    }
+
+
+    // init algorithm
     studentsById = createStudentsById(students);
     long startTime = System.currentTimeMillis();
     numStudents = students.size();  
 
     // Process settings 
-    if (bidirectionalFriendship) {
+    if (bidirectionalFriendships) {
       students = makeFriendsBidirectional(students);
     }
 
-
-    students = sortStudentsByNumFriends(students);
-    res = new ArrayList<Table>();
+    res = new ArrayList<Table>(); // result to be returned
     double score = 0; 
-    double standardDeviation = 0; 
+    double standardDeviation = 1 << 30; // only used for communism
+    ArrayList<Double> lastScores = new ArrayList<Double>(); 
+    double degreesOfChange = 1; // "factor" of relative number of changes to make. "1" changes all items.
+
+    // start searching for solutions
     while (((new Date()).getTime() - startTime) < maxRuntime * 1000) {
-      students = shuffleStudents(students);
-      
+      students = shuffleStudents(students, degreesOfChange);
+
       ArrayList<Table> tablesAttempt = new ArrayList<Table>();
       HashMap<String, Integer> studentsPlaced = new HashMap<String, Integer>();  
 
-      for (Student student : students) { 
-
-        // if student not yet placed
-        if (!studentsPlaced.containsKey(student.getStudentNumber())) {
-
-          Table currTable = new Table(tableSize); 
-          ArrayList<String> shuffledFriends = student.getFriendStudentNumbers();
-          Collections.shuffle(shuffledFriends);
-
-          studentsPlaced.put(student.getStudentNumber(), tablesAttempt.size()); 
-          currTable.addStudent(student);
-          for (String friendID : shuffledFriends) {
-            if (currTable.getNumStudents() == tableSize) {
-              break;
-            }
-            if (!studentsPlaced.containsKey(friendID)) {
-              studentsPlaced.put(friendID, tablesAttempt.size()); 
-              currTable.addStudent(studentsById.get(friendID));
+      int currStu = 0; 
+      while (currStu < numStudents) { // while there are students to be seated
+        // fill table with as many friends of students as possible
+        Table currTable = new Table(tableSize); 
+        while(currTable.getNumStudents() != tableSize && currStu < numStudents) {
+          boolean f = false; 
+          ArrayList<Student> toAdd = new ArrayList<Student>();
+          for (Student s : currTable.getStudents()) {
+            for (String friendID : s.getFriendStudentNumbers()) {
+              if (!studentsPlaced.containsKey(friendID)) {
+                toAdd.add(studentsById.get(friendID)); 
+                studentsPlaced.put(friendID, 1);
+                f = true;
+                break;
+              }
             }
           }
-          tablesAttempt.add(currTable);
-          currTable = new Table(tableSize); 
+          if (f) {
+            currTable.addStudent(toAdd.get(0));
+            continue;
+          }
+
+          if (!studentsPlaced.containsKey(students.get(currStu).getStudentNumber())) {
+            studentsPlaced.put(students.get(currStu).getStudentNumber(), 1);
+            currTable.addStudent(students.get(currStu));
+            currStu += 1; 
+          } else {
+            currStu += 1; 
+          }
         }
+        tablesAttempt.add(currTable);
       }
-      
+
+      // start running comparisons on this arrangement
       double attemptScore = calcSeatingScore(tablesAttempt);
       if (communist) {
         double sd = 0;
@@ -96,20 +184,23 @@ public class SeatingAlg {
         mean /= numStudents; 
 
         sd = standardDeviation(mean, scores);
+
         if (((sd - standardDeviation) / ((sd + standardDeviation) / 2.0) < 0.05 && attemptScore > score) || sd < standardDeviation){
           score = attemptScore; 
           res = tablesAttempt; 
           standardDeviation = sd; 
+          degreesOfChange += 0.5; // high learning rate that is likely too high for communism, because communism isn't supposed to "work" anyway. 
         } 
       } else {
         if (attemptScore > score) {
-          score = attemptScore; 
+          degreesOfChange += 0.05; // degrees of change represents learning rate
+          score = attemptScore;
           res = tablesAttempt;
         }
       }
     }
 
-    res = groupTables(res, tableSize);
+    frame.setVisible(false); // remove loading 
     return res;
   }
 
@@ -124,9 +215,6 @@ public class SeatingAlg {
   * Adjusted median percent satisfaction per person: Adjusted median percent satisfaction (uses same calculations described in adjusted mean percent satisfaction)<br>
   */
   public void outputStatistics() {
-    // standard deviation of percent satisfaction per person
-    // adjusted standard deviation of satisfaction per person
-    // top ten unhappy people 
     int totalFriendships = 0; 
     int friendshipsSatisfied = 0; 
     double meanPercentSatisfaction = 0; 
@@ -161,15 +249,25 @@ public class SeatingAlg {
       }
     }
     meanPercentSatisfaction /= numStudents; 
+    meanAdjustedPercentSatisfaction /= numStudents; 
     System.out.println("\033[1mPercentage of friendships satisfied\033[0m: " + Math.round((friendshipsSatisfied * 1000.0) / totalFriendships) / 10.0);
     System.out.println("\033[1mMean percent satisfaction per person\033[0m: " + Math.round(meanPercentSatisfaction * 1000) / 10.0);
-    System.out.println("\033[1mAdjusted mean percent satisfaction per person\033[0m: " + Math.round(meanAdjustedPercentSatisfaction * 1000) / 100.0);
+    System.out.println("\033[1mAdjusted mean percent satisfaction per person\033[0m: " + Math.round(meanAdjustedPercentSatisfaction * 1000) / 10.0);
     System.out.println("\033[1mMedian percent satisfaction per person\033[0m: " + Math.round(medianPercentSatisfaction[numStudents / 2] * 1000) / 10.0);
     System.out.println("\033[1mAdjusted median percent satisfaction per person\033[0m: " + Math.round(medianAdjustedPercentSatisfaction[numStudents / 2] * 1000) / 10.0);
     System.out.println(""); 
-    System.out.println("\033[1mStandard deviation of percent satisfaction\033[0m: " + Math.round(standardDeviation(meanPercentSatisfaction, medianPercentSatisfaction) * 1000.0) / 1000.0);
-    System.out.println("\033[1mAdjusted standard deviation of percent satisfaction\033[0m: " + Math.round(standardDeviation(meanAdjustedPercentSatisfaction, medianAdjustedPercentSatisfaction) * 1000.0) / 1000.0);
+    System.out.println("\033[1mStandard deviation of percent satisfaction\033[0m: " + Math.round(standardDeviation(meanPercentSatisfaction, medianPercentSatisfaction) * 1000.0) / 10.0);
+    System.out.println("\033[1mAdjusted standard deviation of percent satisfaction\033[0m: " + Math.round(standardDeviation(meanAdjustedPercentSatisfaction, medianAdjustedPercentSatisfaction) * 1000.0) / 10.0);
     System.out.println(""); 
+    return;
+  }
+
+  /**
+  * This method is a builder that sets preferences for your algorithm. If True, then a UI will appear when `generateTables()` is called.<br> Default is true
+   * @param setting should be true if you want user to select settings with UI. Should be false to always use defaults / set settings in TicketingSystem.
+  */
+  public void showUISettings(boolean setting) {
+    showUISettings = setting; 
   }
 
   /**
@@ -177,7 +275,7 @@ public class SeatingAlg {
    * @param setting should be true if you want bidirectional friendships, otherwise false. Default is true.
   */
   public void bidirectionalFriendships(boolean setting) {
-    bidirectionalFriendship = setting; 
+    bidirectionalFriendships = setting; 
   }
 
   /**
@@ -190,13 +288,32 @@ public class SeatingAlg {
     communist = setting; 
   }
 
+  /**
+  * This method is a builder that sets preferences for the algorithm. <br>
+  * If True, then the algorithm will use it's custom equation for calculating participant satisfaction.
+  * @param setting should be true if you want to use the custom equation, otherwise false. Default is true.
+  */
+  public void special(boolean setting) {
+    special = setting; 
+  }
+
+  /**
+  * This method is a builder that sets preferences for the algorithm. <br>
+  * Input should be the approximate number of seconds the algorithm will run. Default is 5.
+  * @param setting approximate number of seconds to run algorithm.
+  */
+  public void maxRuntime(int setting) {
+    maxRuntime = setting; 
+  }
+
   private ArrayList<Student> makeFriendsBidirectional(ArrayList<Student> students) {
+    // for each friendship, if it doesn't exist the other way, make it exist
     for (Student s : students) { 
-      ArrayList<String> friendIDs = s.getFriendStudentNumbers(); 
+      ArrayList<String> friendIDs = s.getFriendStudentNumbers();
       for (String fID : friendIDs) {
         for (int i = 0; i < students.size(); i++) {
-          if (students.get(i).getStudentNumber() == fID) {
-            if (!students.get(i).getFriendStudentNumbers().contains(s.getStudentNumber())){
+          if (students.get(i).getStudentNumber().equals(fID)) {
+            if (!(students.get(i).getFriendStudentNumbers()).contains(s.getStudentNumber())){
               ArrayList<String> updatedFriends = students.get(i).getFriendStudentNumbers(); 
               updatedFriends.add(s.getStudentNumber()); 
               students.get(i).setFriendStudentNumbers(updatedFriends);
@@ -208,78 +325,36 @@ public class SeatingAlg {
     return students; 
   }
 
-  // shuffle students without losing sorting integrity
-  private ArrayList<Student> shuffleStudents(ArrayList<Student> students) { 
-    int currNumFriends = students.get(0).getFriendStudentNumbers().size(); 
-    int prev = 0; // stores index of the last change in number of friends
-    ArrayList<Student> res = new ArrayList<Student>(); 
-    ArrayList<Student> extension = new ArrayList<Student>(); // temporary variable used to add to res
-    
-    for (int i = 0;  i < numStudents; i++) {
-      if (students.get(i).getFriendStudentNumbers().size() != currNumFriends) {
-        extension = new ArrayList<>(students.subList(prev, i));
-        prev = i; 
-        currNumFriends = students.get(i).getFriendStudentNumbers().size();
-        Collections.shuffle(extension); 
-        res.addAll(extension); 
-      }
+
+  private ArrayList<Student> shuffleStudents(ArrayList<Student> students, double factor) { 
+    int changes = (int)((numStudents + 1) / factor); // this many swaps
+
+    Random rand = new Random();
+    for (int i = 0; i < changes; i++) {
+      int first = rand.nextInt(numStudents);
+      int second = rand.nextInt(numStudents);
+      Collections.swap(students, first, second);
     }
-
-    extension = new ArrayList<>(students.subList(prev, numStudents));
-    Collections.shuffle(extension); 
-    res.addAll(extension); 
-
-    return res;
-  }
-
-  // bubble through students to sort them in increasing number of friends
-  private ArrayList<Student> sortStudentsByNumFriends(ArrayList<Student> students) {
-    boolean swap;
-    Student temp;
-
-    for (int i = 0; i < numStudents - 1; i++) {
-      swap = false;
-      for (int j = 0; j < numStudents - i - 1; j++) { 
-        if (students.get(j).getFriendStudentNumbers().size() > students.get(j + 1).getFriendStudentNumbers().size()) {
-          temp = students.get(j); 
-          students.set(j, students.get(j + 1)); 
-          students.set(j + 1, temp); 
-          swap = true; 
+    // also swap order of friends
+    for (Student student : students) {
+      if (student.getFriendStudentNumbers().size() > 1) {
+        changes = (int)((factor + 1) / student.getFriendStudentNumbers().size()); 
+        for (int i = 0; i < changes; i++) {
+          ArrayList<String> friends = student.getFriendStudentNumbers();
+          int first = rand.nextInt(friends.size());
+          int second = rand.nextInt(friends.size());
+          Collections.swap(friends, first, second);
+          student.setFriendStudentNumbers(friends);
         }
       }
-      if (!swap) { 
-        break;
-      }
     }
-    return students; 
+    return students;
   }
 
   private HashMap<String, Student> createStudentsById(ArrayList<Student> students) {
     HashMap<String, Student> res = new HashMap(); 
     for (Student student : students) {
       res.put(student.getStudentNumber(), student);
-    }
-    return res;
-  }
-
-  private ArrayList<Table> groupTables(ArrayList<Table> tables, int tableSize) {
-    ArrayList<Table> res = new ArrayList<Table>();
-    Table temp = new Table(tableSize);
-    int tempSum = 0;
-
-    for (Table t : tables) { 
-      if (tempSum + t.getNumStudents() <= tableSize) {
-        temp.addStudents(t.getStudents()); 
-        tempSum += t.getNumStudents();
-      } else {
-        res.add(temp); 
-        temp = new Table(tableSize);
-        temp.addStudents(t.getStudents());
-        tempSum = t.getNumStudents();
-      }
-    }
-    if (temp.getNumStudents() > 0) {
-        res.add(temp);
     }
     return res;
   }
@@ -306,14 +381,23 @@ public class SeatingAlg {
 
   private double calcPersonScore(Student student, int friendsAtTable) {
     int totalFriends = student.getFriendStudentNumbers().size(); 
-    double happinessByNumFriends = -0.75 / (2 * friendsAtTable + 1) + 0.75;
-    double adjustmentByPercentFriends = 0; 
-    if (totalFriends > 0) { 
-      adjustmentByPercentFriends = -1/4.0 * (friendsAtTable / (totalFriends * 1.0) - 1) * (friendsAtTable / (totalFriends * 1.0) - 1) + 0.25;
-    } else {
-      adjustmentByPercentFriends = 1;
+    if (special) {
+      double happinessByNumFriends = -0.75 / (2 * friendsAtTable + 1) + 0.75;
+      double adjustmentByPercentFriends = 0; 
+      if (totalFriends > 0) { 
+        adjustmentByPercentFriends = -1/4.0 * (friendsAtTable / (totalFriends * 1.0) - 1) * (friendsAtTable / (totalFriends * 1.0) - 1) + 0.25;
+      } else {
+        adjustmentByPercentFriends = 1;
+      }
+      return happinessByNumFriends + adjustmentByPercentFriends;
     }
-    return happinessByNumFriends + adjustmentByPercentFriends;
+
+    // not special
+    if (totalFriends == 0) {
+      return 1; 
+    } else {
+      return (friendsAtTable / totalFriends * 1.0);
+    }
   }
 
   private double standardDeviation(double mean, double[] values) {
--- prom_semi/TicketingSystem.java
+++ prom_semi/TicketingSystem.java
@@ -1,79 +1,131 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.io.PrintWriter;
 
-public class TicketingSystem { 
-  static int numStudents = 10; 
-  static int maxPerTable = 7; 
-  static String[] possibleDietaryRestrictions = {"vegetarian", "vegan", "peanut allergy", "lactose intolerant", "halal", "gluten free"};
-  
-  public static void main(String[] args) {
-    ArrayList<Student> mockStudents = new ArrayList<Student>(); 
-    Random rand = new Random();
+public class TicketingSystem {
+  static HashSet<String> picked = new HashSet<String>();
+    final static int NUM_STUDENTS = 100, STUDENTS_PER_TABLE = 10, EDGES_PER_STUDENT = 5;
+    static Random rng = new Random();
+    static boolean[][] friend;
+    static int[] friendCnt;
+    static int totFriends=0;
 
-    // Preset student numbers
-    String[] studentNumbers = new String[numStudents]; 
-    for (int i = 0; i < numStudents; i++) { 
-      studentNumbers[i] = Integer.toString(rand.nextInt(100000000));
+  public static void main(String[] args) throws FileNotFoundException {
+    SeatingAlg sa = new SeatingAlg();
+    ArrayList<Student> students = new ArrayList<Student>();
+    genFile(5);
+    Scanner in = new Scanner(new File("kust.txt"));
+    while (in.hasNext()) {
+      students.add(parse(in.nextLine()));
     }
-
-    // Start generating students
-    for (int i = 0; i < numStudents; i++) { 
-
-      // Create fake friends list
-      int lenMockFriends = rand.nextInt(8); 
-      ArrayList<String> mockFriends = new ArrayList<String>();
-      for (int j = 0; j < lenMockFriends; j++) { 
-        mockFriends.add(studentNumbers[rand.nextInt(studentNumbers.length)]); 
-      }
-
-      // Create fake dietary restrictions
-      int lenMockDietaryRestrictions = rand.nextInt(3); 
-      ArrayList<String> mockDietaryRestrictions = new ArrayList<String>();
-      for (int j = 0; j < lenMockDietaryRestrictions; j++) { 
-        mockDietaryRestrictions.add(possibleDietaryRestrictions[rand.nextInt(possibleDietaryRestrictions.length)]); 
-      }
-
-      // add the student
-      mockStudents.add(new Student(
-        randName(rand, true), 
-        studentNumbers[i], 
-        mockDietaryRestrictions,
-        mockFriends    
-      )); 
-    }
-
-    // run algorithm
-    SeatingAlg mockSeatingAlg = new SeatingAlg();
-      mockSeatingAlg.bidirectionalFriendships(false);
-    ArrayList<Table> tables = mockSeatingAlg.generateTables(mockStudents, 7); 
-
+    sa.special(false);
+    // sa.bidirectionalFriendships(true);
+    sa.maxRuntime(2);
+    sa.showUISettings(false);
+    ArrayList<Table> tables = sa.generateTables(students, STUDENTS_PER_TABLE);
 
     // output tables with names and student numbers
     for (Table table : tables) { 
-      ArrayList<Student> students = table.getStudents();
-      for (Student student : students) { 
+      ArrayList<Student> s = table.getStudents();
+      for (Student student : s) { 
         System.out.println(student.getName() + " " + student.getStudentNumber());
       }
       System.out.println("----");
     }
 
-    mockSeatingAlg.outputStatistics();
+
+    sa.outputStatistics();
+
+
   }
 
-  private static String randName(Random random, boolean isFirst) {
-
-    String letters = "pyfgcrlaoeuidhtnsqjkxbmwvz"; 
-    int nameLength = random.nextInt(6) + 5;
-    String res = ""; 
-    for (int i = 0; i < nameLength; i++) {
-      res += letters.charAt(random.nextInt(26));
+  private static Student parse(String in) {
+    String[] part = in.split(",");
+    //String[] diet = part[2].split(" ");
+    String[] friends;
+    ArrayList<String> cust = new ArrayList<String>();
+    if (part.length > 1){
+      friends = part[1].split(" ");
+      cust = new ArrayList(Arrays.asList(friends));
     }
-    res = res.substring(0, 1).toUpperCase() + res.substring(1);
-    if (isFirst){
-      return res + " " + randName(random, false);
-    } else { 
-      return res;
-    }
+    //}
+//      return new Student(part[0], part[1], new ArrayList(Arrays.asList(diet)), cust);
+      return new Student("", part[0], new ArrayList(), cust);
   }
+
+    // generates student numbers
+    public static String genNumber() {
+        String number = "";
+        for (int j = 0; j < 9; ++j)
+            number += Character.toString((char) (rng.nextInt(10) + '0'));
+        return number;
+    }
+
+  public static void genFile(int bidirChance) throws FileNotFoundException {
+
+    friend = new boolean[NUM_STUDENTS][NUM_STUDENTS];
+    friendCnt = new int[NUM_STUDENTS];
+
+    // generates students
+    ArrayList<String> students = new ArrayList<String>();
+    ArrayList<Student> stus=new ArrayList<Student>();
+    for (int i = 0; i < NUM_STUDENTS; ++i) {
+        String number = genNumber();
+        while (picked.contains(number))
+            number = genNumber();
+        students.add(number);
+    }
+
+    // generates friends
+        for (int i = 0; i < NUM_STUDENTS; ++i) {
+            // number of friends current person has
+            int numFriends = Math.min(rng.nextInt(EDGES_PER_STUDENT + 1),NUM_STUDENTS-1);
+            totFriends += numFriends;
+
+            // list storing person's friends
+            ArrayList<String> friends = new ArrayList<String>();
+
+            // checks for existing friends
+            for (int j = 0; j < NUM_STUDENTS; ++j) {
+                if (friend[i][j]) {
+                    friends.add(students.get(j));
+                }
+            }
+
+            // generates remaining friends
+            for (int j = friends.size(); j < numFriends; ++j) {
+                // generates current friend,if friendship is mutual
+                int idx = rng.nextInt(NUM_STUDENTS), bidir = rng.nextInt(bidirChance);
+                while (idx == i || friend[i][idx]
+                        || (bidir > 0 && (friendCnt[idx] >= EDGES_PER_STUDENT || friend[idx][i])))
+                    idx = rng.nextInt(NUM_STUDENTS);
+
+                // if friendship is mutual generates other edge
+                if (bidir > 0) {
+                    friend[idx][i] = true;
+                    ++friendCnt[idx];
+                }
+
+                // adds friend
+                friend[i][idx] = true;
+                ++friendCnt[i];
+                friends.add(students.get(idx));
+            }
+            stus.add(new Student("A",students.get(i),new ArrayList<String>(),friends));
+        }
+
+        PrintWriter output = new PrintWriter(new File("kust.txt"));
+        for (int i = 0; i < stus.size(); ++i) {
+            output.print(stus.get(i).getStudentNumber() + ",");
+            for (String j : stus.get(i).getFriendStudentNumbers())
+                output.print(j + " ");
+            output.println();
+        }
+        output.close();
+    }
 }
